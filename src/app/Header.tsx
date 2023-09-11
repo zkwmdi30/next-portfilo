0c3f259cf2abd1b12ccb295d6c965db2799c4047
이명필 <mplee@thewc.co.kr>
2023-09-11 11:47:16 +0900

    페이지 추가

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -15,7 +15,9 @@ export default function Header() {
   return (
     <header className="p-8 w-full">
       <div className={scrollPosition < 150 ? 'original_header' : 'change_header'}>
-        <a className="text-yellow-500 text-2xl font-medium">portfolio</a>
+        <a className="text-yellow-500 text-2xl font-medium" href="/">
+          portfolio
+        </a>
       </div>
     </header>
   );
