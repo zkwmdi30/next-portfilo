266961fb019acfd16a1a9d7cbbce1b1724c68cbd
이명필 <mplee@thewc.co.kr>
2023-11-16 14:46:12 +0900

    사진 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+import Image from 'next/image';
 import Introduce from './Introduce';
 import Skills from './Skills';
 import Experiences from './Experiences';
 import Educations from './Educations';
+import './page.css';
 
 export default function Home() {
   return (
     <main className="max-w-4xl pt-28 text-gray-300 mx-auto">
       <section className="text-yellow-500 text-7xl font-bold m-16 leading-normal">
-        <p>안녕하세요.</p>
+        <div className="flex flex-row items-center">
+          <div className="image-container mr-4">
+            <Image className="face" src="/images/face.JPG" width={60} height={60} alt="얼굴" />
+          </div>
+          안녕하세요.
+        </div>
         <p>프론트엔드 개발자</p>
         <p>이명필입니다.</p>
       </section>
