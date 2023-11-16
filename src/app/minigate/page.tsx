75b7152e388b86f0a8ba6342ee520e5f79fc50f2
이명필 <mplee@thewc.co.kr>
2023-11-16 09:51:55 +0900

    이력서 내용 수정

--- src/app/minigate/page.tsx
+++ src/app/minigate/page.tsx
@@ -5,11 +5,29 @@ export default function Minigate() {
   return (
     <div className="flex min-h-screen flex-col items-center">
       <main className="pt-28 text-gray-300">
-        <section id="font-bold m-32 leading-normal">미니게이트</section>
+        <section id="font-bold m-32 leading-normal">
+          <div className="">
+            <h1 className="text-3xl font-bold text-center mb-6">미니게이트</h1>
+            <div className="bg-white shadow-md rounded-lg p-6 text-gray-600">
+              <h2 className="text-2xl font-semibold mb-4">플랫폼개발실 / 대리</h2>
+              <p className="font-medium  mb-4">
+                <strong>2021.08 - 2022.03</strong>
+              </p>
+
+              <h3 className="mt-10 font-bold">Gnb 페럴렉스 교육 SI 수행</h3>
+              <ul className="list-disc pl-5 space-y-2 mt-10">
+                <li>개발스택: Spring Boot, Vue, Nuxt 개발</li>
+                <li>프론트엔드 주요 프레임워크 세팅 및 설계</li>
+                <li>주요 로직 구현 : 로그인, 라우터, 인증, 녹음 등</li>
+                <li>서버 세팅 및 백엔드 개발</li>
+              </ul>
+            </div>
+          </div>
+        </section>
       </main>
 
-      <div className="mt-32">
-        <img src="https://lh3.google.com/u/1/d/1K7gfUVjFiHCtXjA3joTZ0pwpNgnaKhse=w4486-h3176-iv1" alt="포트폴리오 사진" />
+      <div className="relative w-full h-32 my-32">
+        <Image className="mt-8" src="/images/13_1400.png" layout="responsive" width={500} height={300} alt="고객정보" />
       </div>
     </div>
   );
--- src/app/thewc/page.tsx
+++ src/app/thewc/page.tsx
@@ -8,7 +8,7 @@ export default function Thewc() {
       <main className="pt-28 text-gray-300">
         <section id="font-bold m-32 leading-normal">
           <div className="">
-            <h1 className="text-3xl font-bold text-center mb-6">더화이트커뮤니케이션(주) 경력</h1>
+            <h1 className="text-3xl font-bold text-center mb-6">더화이트커뮤니케이션 경력</h1>
             <div className="bg-white shadow-md rounded-lg p-6 text-gray-600">
               <h2 className="text-2xl font-semibold mb-4">플랫폼서비스실 / 팀원</h2>
               <p className="font-medium  mb-4">
@@ -17,40 +17,40 @@ export default function Thewc() {
 
               <h3 className="mt-10 font-bold">프론트엔드 시니어급으로 근무</h3>
               <ul className="list-disc pl-5 space-y-2 mt-10">
-                <li>사내 CS 통합지원 플랫폼 개발 [Vue.js]</li>
-                <li>플랫폼 주요 기능 제작 - 웹소켓을 통한 채팅 상담, 챗봇 등</li>
-                <li>주요 기능 개발 시 안정성에 극히 신경 써서 수행</li>
-                <li>깃허브 PR 및 코드 리뷰 문화 습득</li>
-                <li>Vue의 다양한 기능들 (mixin, extends, v-slot 등) 활용</li>
+                <li>Vue.js</li>
+                <li>프론트엔드 개발 리딩</li>
+                <li>주요 기능 개발 (채널톡 서비스, 챗봇 등)</li>
               </ul>
 
-              <h3 className="mt-10 font-bold">배포라인 작업 및 DevOps 관련 업무</h3>
+              <h3 className="mt-10 font-bold">CI/CD 환경 구축</h3>
               <ul className="list-disc pl-5 space-y-2 mt-10">
-                <li>데브옵스 담당자 미구인 상태에서 배포라인 작업 수행</li>
-                <li>배포 라인 관련 문제 수정 (Jenkins, AWS Deploy, S3)</li>
-                <li>프론트 빌드 자동화 및 메신저 툴 연동</li>
-                <li>배포 시 E2E 테스트 관련 작업 수행</li>
+                <li>Jenkins, AWS Deploy, S3</li>
+                <li>프론트엔드 빌드 자동화</li>
+                <li>배포 E2E 연동</li>
+                <li>배포 라인 관련 문제 수정</li>
               </ul>
 
-              <h3 className="mt-10 font-bold">멀티브랜드 리뉴얼 프로젝트</h3>
+              <h3 className="mt-10 font-bold">멀티 브랜드 리뉴얼 프로젝트</h3>
               <ul className="list-disc pl-5 space-y-2 mt-10">
-                <li>메인 키값 배열화 및 프로젝트 전반 리펙토링 작업</li>
-                <li>화면 로딩 시 리스트로 변경, 비동기 처리 부분 확장</li>
-                <li>무리한 일정 속 프론트팀 리딩 및 프로젝트 완수</li>
+                <li>프론트엔드 개발 리딩</li>
+                <li>프로젝트 전체 리펙토링</li>
+                <li>레거시 코드 교체 및 소스 효율화</li>
+                <li>주요 기능 개발 (계정, 인증, 로그인, 채팅 상담, 계정 관리,상담 내역 관리)</li>
               </ul>
 
-              <h3 className="mt-10 font-bold">E2E 테스트 도입 및 단위 테스트 스터디 진행</h3>
+              <h3 className="mt-10 font-bold">개발 문화 도입</h3>
               <ul className="list-disc pl-5 space-y-2 mt-10">
-                <li>레거시 코드 문제로 TDD 및 단위 테스트 도입 추진</li>
-                <li>Cypress를 이용한 엔드포인트 테스트 구성</li>
-                <li>단위 테스트에 대한 팀 내 스터디 리드 및 진행</li>
+                <li>jest, cypress, github</li>
+                <li>jest 단위 테스트 스터디 리딩</li>
+                <li>cypress 엔드포인드 테스트 도입</li>
+                <li>깃허브 PR / 코드리뷰 문화 도입</li>
+                <li>사내 cypress 강의 및 스터디 리딩</li>
               </ul>
 
-              <h3 className="mt-10 font-bold">콜 상담데스크 개선 프로젝트 (PM)</h3>
+              <h3 className="mt-10 font-bold">콜 상담데스크 개선 프로젝트</h3>
               <ul className="list-disc pl-5 space-y-2 mt-10">
-                <li>콜 상담데스크의 주요 문제점 개선 주도 및 PM으로 프로젝트 참여</li>
-                <li>전반적인 시스템 리펙토링 및 신규 기능 적용</li>
-                <li>클라이언트 로그(IndexedDB) 효율화 작업으로 중복 로그 감소 및 데이터 추적 개선</li>
+                <li>프로젝트 전체 리펙토링</li>
+                <li>클라이언트 로그 (IndexedDB) 효율화</li>
               </ul>
             </div>
           </div>
